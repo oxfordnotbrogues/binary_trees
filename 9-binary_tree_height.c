2aa75717f67e5e627d216e70414509496e27892f
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-11-03 14:52:48 -0700

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,6 +22,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			j = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 			k = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((j > k) ? k : j);
+		return ((j > k) ? j : k);
 	}
 }
